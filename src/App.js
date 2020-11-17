4dd49ad499a68ba18b5c962f23f97b77f1a3eeb8
Jorgen <ybeliaev@gmail.com>
2020-11-17 20:21:31 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import {  
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <Switch>
+      <Route exact path="/"><h1>Hi, this is home page</h1></Route>    
+      <Route exact path="/starred"><h1>Hi, this is starred page</h1></Route>
+      <Route><h1>Hi, this is 404 page</h1></Route>
+  </Switch>
   );
 }
 
