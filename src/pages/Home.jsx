75106cc9f5f0afd5b86f192e3edbf0f14066dcfc
Jorgen <ybeliaev@gmail.com>
2020-11-18 15:25:39 +0200

    add input & fetch results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,42 @@
+import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
 
 const Home = () => {
-  return <MainPageLayout>This is Home!</MainPageLayout>;
+  const [input, setInput] = useState("");
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onSearch = () => {
+    // from https://www.tvmaze.com/api :
+    // https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then((r) => r.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
